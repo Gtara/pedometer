0444acdfbced399cd34c9aaa7b43ee9160038f79
George <32527244+Gtara@users.noreply.github.com>
2021-03-12 15:47:10 +0200

    added steps page

--- steps/models.py
+++ steps/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Steps(models.Model):
     steps = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(50000)])
     date = models.DateField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (str(self.owner) + " " + str(self.date) + " " + str(self.steps))
--- steps/views.py
+++ steps/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Steps
+from django.shortcuts import redirect
 
 # Create your views here.
 
+@login_required
 def home(request):
-    return render(request, 'steps/home.html')
+    if request.method == 'POST':
+        if request.POST['steps'] and request.POST['date']:
+            inst = Steps()
+            inst.steps = request.POST['steps']
+            inst.date = request.POST['date']
+            inst.owner = request.user
+            inst.save()
+            return redirect('home')
+        else:
+            return render(request, 'steps/home.html', {'error': 'All fields are required.'})
+    else:        
+        return render(request, 'steps/home.html')
